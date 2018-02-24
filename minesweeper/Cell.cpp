c6166f192c1b544ec5d12d434c99f983f805decb
Eulerd <dicenull.dev@gmail.com>
2018-02-24 15:51:00 +0900

    各セルを管理するクラスを追加

---
+++ minesweeper/Cell.cpp
@@ -0,0 +1,104 @@
+#include "Cell.h"
+#include "Field.h"
+
+
+Cell::Cell(bool is_mine, int count)
+{
+	_count = count;
+	_state = CellState::None;
+	_is_mine = is_mine;
+	_is_open = false;
+	_rect = Rect(Field::CellSize);
+}
+
+
+Cell::Cell()
+	: Cell::Cell(true, -1) {}
+
+Cell::~Cell()
+{
+}
+
+bool Cell::Open()
+{
+	_is_open = true;
+
+	return _is_mine;
+}
+
+void Cell::Draw() const
+{
+	_rect.draw(_is_open ? Palette::Gray : Palette::Darkgray);
+	_rect.drawFrame(0.5, 0, Palette::Lightgrey);
+
+	if (_is_open)
+	{
+		if (_is_mine)
+		{
+			TextureAsset(L"mine").drawAt(_rect.center);
+		}
+		else if(_count > 0)
+		{
+			FontAsset(L"cell")(ToString(_count)).drawCenter(_rect.center, count_color());
+		}
+	}
+	else
+	{
+		switch (_state)
+		{
+		case CellState::Flag:
+			TextureAsset(L"flag").drawAt(_rect.center);
+			break;
+		case CellState::Question:
+			TextureAsset(L"question").drawAt(_rect.center);
+			break;
+		}
+	}
+}
+
+void Cell::ChangeNextState()
+{
+	switch (_state)
+	{
+	case CellState::None:
+		_state = CellState::Flag;
+		break;
+	case CellState::Flag:
+		_state = CellState::Question;
+		break;
+	case CellState::Question:
+	default:
+		_state = CellState::None;
+		break;
+	}
+}
+
+void Cell::SetPos(Point pos)
+{
+	_rect.setPos(pos);
+}
+
+Color Cell::count_color() const
+{
+	switch (_count)
+	{
+	case 1:
+		return Palette::Blue;
+	case 2:
+		return Palette::Green;
+	case 3:
+		return Palette::Red;
+	case 4:
+		return Palette::Darkblue;
+	case 5:
+		return Palette::Brown;
+	case 6:
+		return Palette::Mediumseagreen;
+	case 7:
+		return Palette::Black;
+	case 8:
+		return Palette::Darkgray;
+	default:
+			return Palette::White;
+	}
+}
---
+++ minesweeper/Cell.h
@@ -0,0 +1,58 @@
+#pragma once
+#include <Siv3D.hpp>
+#include "CellState.h"
+
+class Cell
+{
+public:
+	/// <summary>
+	/// セル内にmineありで初期化
+	/// </summary>
+	Cell();
+
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	/// <param name="is_mine">このセルがmineを持っているか</param>
+	/// <param name="count">セルの周りにあるmineの数</param>
+	Cell(bool is_mine, int count);
+	~Cell();
+
+	/// <summary>
+	/// セルを描画します
+	/// </summary>
+	void Draw() const;
+
+	/// <summary>
+	/// セルを開けmineがあるか確認します
+	/// </summary>
+	/// <returns>mineがあったかどうか</returns>
+	bool Open();
+
+	/// <summary>
+	/// セルを次の状態に変更する
+	/// </summary>
+	void ChangeNextState();
+
+	/// <summary>
+	/// セルの場所を変更します
+	/// </summary>
+	/// <param name="pos">移動先の場所</param>
+	void SetPos(Point pos);
+
+private:
+	bool _is_mine;
+	bool _is_open;
+	CellState _state;
+	int _count;
+	Rect _rect;
+
+private:
+	/// <summary>
+	/// 数に応じた色を取得する
+	/// </summary>
+	/// <returns>周りのmineの数に応じた色を返す</returns>
+	Color count_color() const;
+		
+};
+
