17c719279577e420833dbb7637dcca31ae99b521
Eulerd <dicenull.dev@gmail.com>
2018-02-24 15:53:16 +0900

    フィールドを描画し、セルを開けると周囲のmineの数かmineが描画されるようにした。

--- minesweeper/Main.cpp
+++ minesweeper/Main.cpp
@@ -1,14 +1,37 @@
 ﻿
 # include <Siv3D.hpp>
+#include "Field.h"
 
 void Main()
 {
-	const Font font(30);
+	int c_size = Field::CellSize;
+	FontAsset::Register(L"cell", Field::CellSize / 3);
+	TextureAsset::Register(L"flag", L"Images/flag.png");
+	TextureAsset::Register(L"mine", L"Images/mine.png");
+	TextureAsset::Register(L"question", L"Images/question.png");
+	TextureAsset(L"flag").resize(Size(c_size - 1, c_size - 1));
+	TextureAsset(L"mine").resize(Size(c_size - 1, c_size - 1));
+	TextureAsset(L"question").resize(Size(c_size - 1, c_size - 1));
+
+
+	Point origin{ 0,0 };
+	Field field{ Size(16, 16), 10, origin };
 
 	while (System::Update())
 	{
-		font(L"ようこそ、Siv3D の世界へ！").draw();
+		// マウスがクリックしたセルの座標
+		Point pos = Point(Mouse::Pos() - origin) / Field::CellSize;
 
-		Circle(Mouse::Pos(), 50).draw({ 255, 0, 0, 127 });
+		if (Input::MouseL.clicked)
+		{			
+			field.OpenCell(pos);
+		}
+
+		if (Input::MouseR.clicked)
+		{
+			field.ChangeCellState(pos);
+		}
+
+		field.Draw();
 	}
 }
